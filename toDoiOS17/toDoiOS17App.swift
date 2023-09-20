f009322e587039dbfe851d7ab4d4bc7f0d3535ec
Berkay <berkaysutlu94@gmail.com>
2023-09-20 13:18:52 +0300

    Initial Commit

---
+++ toDoiOS17/toDoiOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  toDoiOS17App.swift
+//  toDoiOS17
+//
+//  Created by Berkay Sutlu on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct toDoiOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
